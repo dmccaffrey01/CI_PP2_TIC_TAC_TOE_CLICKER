9468e3e779079dc9d6707f2e047dc7252d1ae709
Dillon Mc Caffrey <dmccaffrey01@gmail.com>
2023-01-10 18:37:53 +0000

    Fix styling for home buttons and random quote sections

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,10 +42,7 @@ const QUOTEBANK = [
     }, { 
         quote: "You miss 100% of the shots you don't take.",
         author: "Wayne Gretzky"
-    }, {
-        quote: "I've missed more than 9000 shots in my career. I've lost almost 300 games. 26 times I've been trusted to take the game winning shot and missed. I've failed over and over and over again in my life. And that is why I succeed.",
-        author: "Michael Jordan"
-    }, {
+    },{
         quote:"Every strike brings me closer to the next home run.",
         author:"Babe Ruth"
     } 
